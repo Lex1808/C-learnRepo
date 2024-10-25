2f4d403e36e9c07bd7bcc12a80fa8494f56b91b9
Oleksandr <112832902+Lex1808@users.noreply.github.com>
2024-10-25 23:03:50 +0300

    Create repeatwords.cpp

---
+++ babySteps/repeatwords.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string current;
+    string previous;
+    int word_number = 0;
+    int repeat = 0;
+
+    while(cin >> current && current != "q") {
+        word_number++;
+        if (current == previous) {
+            cout << "Word number " << word_number << " repeat: " << current << "\n";
+            repeat++;
+        }
+        previous = current;
+        
+    }
+    cout << "Repeat counting equel: " << repeat;
+
+    return 0;
+}
