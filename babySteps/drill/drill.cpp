6ecb3e9b3b51def6d93f96b0439a7b152c1c324b
Oleksandr <112832902+Lex1808@users.noreply.github.com>
2024-11-11 23:43:27 +0200

    Update drill.cpp

--- babySteps/drill/drill.cpp
+++ babySteps/drill/drill.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
 #include <limits>
 
+double read_number() {
+    double num;
+    while (!(std::cin >> num)) { 
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "Invalid input. Please enter a number: ";
+    }
+    return num;
+}
+
 int main()
 {
-    double point1;
-    double point2;
+    double number;
     char terminator;
+    double small_num;
+    double large_num;
+
+    std::cout << "Enter number: ";
+    number = read_number();
+    small_num = large_num = number;
     do
     {
         std::cout << "Enter two integers: ";
@@ -15,26 +30,29 @@ int main()
         }
         std::cin.putback(terminator);
 
-        std::cin >> point1 >> point2;
-        if(std::cin.fail()) {
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            continue;
-        }
+        number = read_number();
 
-        double large = point1 > point2 ? point1: point2;
-        double smaller = point1 < point2 ? point1: point2;
-        if (point1 == point2) {
-            std::cout << "The numbers: " << point1 << " and " << point2 << " are equel" << std::endl;
+        if(number < small_num) {
+            small_num = number;
+            std::cout << small_num << " the smallest so far" << std::endl;
+        } else if(number > large_num) {
+            large_num = number;
+            std::cout << large_num << " the largest so far" << std::endl;
         } else {
-            std::cout << "The larger value is: " << large << std::endl;
-            std::cout << "The smaller value is: " << smaller << std::endl;
-            if ((large - smaller) <= (1.0/100)) {
-                std::cout << "The numbers: " << point1 << " and " << point2 << " are almost equal" << std::endl;
-            } 
+            std::cout << number << std::endl;
         }
 
     } while (true);
+
+    if (small_num == large_num) {
+        std::cout << "The numbers: " << small_num << " and " << large_num << " are equel" << std::endl;
+    } else {
+        std::cout << "The larger value is: " << large_num << std::endl;
+        std::cout << "The smaller value is: " << small_num << std::endl;
+        if ((large_num - small_num) <= (1.0/100)) {
+            std::cout << "The numbers: " << large_num << " and " << small_num << " are almost equal" << std::endl;
+        } 
+    }
 
     return 0;
 }
